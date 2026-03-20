8a03074c59ac4f4ec4c2ab0e2259d8e4a3070030
tiswas247 <tiswas247@users.noreply.github.com>
2026-03-20 12:39:07 +0000

    removed unused stuff

--- crates/db-gen/src/queries.rs
+++ crates/db-gen/src/queries.rs
@@ -1,11 +1,5 @@
 // This file was generated with `clorinde`. Do not modify.
 
-pub mod agents;
 pub mod auth;
-pub mod billing;
-pub mod channels;
-pub mod channels_list;
-pub mod integration_connections;
 pub mod integrations;
 pub mod orgs;
-pub mod providers;
